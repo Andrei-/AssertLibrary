b126f59d2abf58456e151df7cf6eaca004dfd798
Andrei <andrei.vleju@gmail.com>
2017-11-30 23:28:46 +0100

    Initial seed

---
+++ Core/Assert.Core/Public Methods/IsNotNullOrWhiteSpace.cs
@@ -0,0 +1,24 @@
+﻿namespace AssertLibrary
+{
+    using System.Globalization;
+
+    public static partial class Assert
+    {
+        /// <summary>
+        /// Asserts that the given string is null or empty.
+        /// </summary>
+        /// <param name="value">String to check</param>
+        /// <param name="variableName">Variable name to include in the error message (optional)</param>
+        public static void IsNotNullOrWhiteSpace(string value, string variableName = null)
+        {
+            Check(!string.IsNullOrWhiteSpace(value), IsNotNullOrWhiteSpaceMessage(variableName));
+        }
+
+        private static string IsNotNullOrWhiteSpaceMessage(string variableName)
+        {
+            return variableName == null ?
+                "Should have a value other then null or white space at this point." :
+                string.Format(CultureInfo.CurrentCulture, "\"{0}\" should have a value other then null or white space at this point.", variableName);
+        }
+    }
+}
---
+++ Core/Tests/IsNotNullOrWhiteSpace.cs
@@ -0,0 +1,27 @@
+﻿namespace AssertLibrary.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Assert = AssertLibrary.Assert;
+
+    [TestClass]
+    public class IsNotNullOrWhiteSpace
+    {
+        [TestMethod]
+        public void IsNotNullOrEmpty_With_Valid_String()
+        {
+            Assert.IsNotNullOrWhiteSpace("test");
+        }
+
+        [TestMethod, ExpectedException(typeof(AssertException))]
+        public void IsNotNullOrEmpty_With_Null()
+        {
+            Assert.IsNotNullOrWhiteSpace(null);
+        }
+
+        [TestMethod, ExpectedException(typeof(AssertException))]
+        public void IsNotNullOrEmpty_With_EmptyString()
+        {
+            Assert.IsNotNullOrWhiteSpace(string.Empty);
+        }
+    }
+}
